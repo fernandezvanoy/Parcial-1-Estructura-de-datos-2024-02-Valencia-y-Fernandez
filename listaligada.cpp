bbc4a90ccf37ee49eee0b3321c4d9d36c0d7ddd5
valenciavm <valenciavm05@gmail.com>
2024-08-21 09:09:52 -0500

    comentarios

--- listaligada.cpp
+++ listaligada.cpp
@@ -3,6 +3,7 @@
 
 ListaLigada::ListaLigada() : cabeza(nullptr), final(nullptr) {}
 
+//Constructor de la lista ligada, establece cabeza vacía por defecto
 ListaLigada::~ListaLigada() {
     Nodo* actual = cabeza;
     while (actual != nullptr) {
@@ -12,13 +13,25 @@ ListaLigada::~ListaLigada() {
     }
 }
 
+//Retorno para la variable duración (long long)
 long long ListaLigada::sort(int select){
     using namespace std::chrono;
 
+
+    //decltype transforma el tipo de dato
+    //high_resolution_clock::now() inicia el conteo de tiempo y calcula el tiempo total con la diferencia
+    //duration_cast<miliseconds> transforma el tiempo obtenido a milisegundos
+    //duración obtiene el tipo de dato resultante de delctype(...)
+    //.count() accede al valor almacenado de dicha duración
     decltype(duration_cast<milliseconds>(high_resolution_clock::now() - high_resolution_clock::now()).count()) duracion = 0;
 
 
+    //selecciona entre los métodos de ordenamiento:
+    //Inicia los tiempos y posteriormente ejecuta un método de ordenamiento interno de la clase lista ligada
+    //al inicio y al final se usan marcas de tiempo para calcular su diferencia
     if(select == 1){
+
+
         auto inicio = high_resolution_clock::now();
         this->bubblesort();
         auto fin = high_resolution_clock::now();
@@ -46,6 +59,8 @@ long long ListaLigada::sort(int select){
 
 }
 
+//permite agregar un nodo a la lista
+//busca el ultimo nodo cuyo apuntador de siguiente esté vacío
 void ListaLigada::agregar(const Producto& producto, int index) {
     Nodo* nuevoNodo = new Nodo(producto, index);
     if(cabeza == nullptr){
@@ -62,6 +77,8 @@ void ListaLigada::agregar(const Producto& producto, int index) {
     final = nuevoNodo;
 }
 
+//Imprime los nodos de la lista accediendo a los datos que almacenan
+//recorre uno por uno hasta llegar a nulo
 void ListaLigada::imprimir() const {
     Nodo* actual = cabeza;
     while (actual != nullptr) {
@@ -70,6 +87,7 @@ void ListaLigada::imprimir() const {
     }
 }
 
+//Intercambia los datos que almacenan dos nodos con ayuda de una variable temporal
 void ListaLigada::intercambiar(Nodo* nodo1, Nodo* nodo2) {
     Producto temp = nodo1->data;
     nodo1->data = nodo2->data;
@@ -80,6 +98,10 @@ void ListaLigada::intercambiar(Nodo* nodo1, Nodo* nodo2) {
 
 }
 
+//Método de bubble sort
+//Compara los atributos almacenados en datos y segun la condición intercambia estos datos almacenados en los nodos
+//lleva los datos mas grandes a los últimos nodos
+//una vez no realice ningun cambio termina el ciclo lo que significa la lista ya está ordenada
 void ListaLigada::bubblesort() {
     if(cabeza == nullptr) return;
 
@@ -100,6 +122,8 @@ void ListaLigada::bubblesort() {
 
 }
 
+
+//getters y setters de cabeza y cola
 Nodo* ListaLigada::getFinal(){
     return final;
 }
@@ -108,10 +132,12 @@ Nodo* ListaLigada::getCabeza(){
     return cabeza;
 }
 
+//funció auxiliar para ejecutar la función recursiva
 void ListaLigada::quicksort() {
     quicksortrec(cabeza, final);
 }
 
+//función recursiva quicksort, se usan dos indicadores izquierda y derecha 
 void ListaLigada::quicksortrec(Nodo* izquierda, Nodo* derecha) {
     if (derecha != nullptr && izquierda != derecha && izquierda != derecha->siguiente) {
         Nodo* pivote = dividir(izquierda, derecha);
@@ -120,25 +146,36 @@ void ListaLigada::quicksortrec(Nodo* izquierda, Nodo* derecha) {
     }
 }
 
+//Divide la lista
 Nodo* ListaLigada::dividir(Nodo* izquierda, Nodo* derecha) {
-    Nodo* pivote = derecha;
-    Nodo* i = izquierda->anterior;
+    Nodo* pivote = derecha; //se elige ultimo nodo como pivote
+    Nodo* i = izquierda->anterior;//inicializa i como el nodo anterior a la izquierda
 
+    //recorre la lista desde izquierda hasta antes de la derecha
     for (Nodo* j = izquierda; j != derecha; j = j->siguiente) {
         if (j->data.getAtributo() <= pivote->data.getAtributo()) {
-            i = (i == nullptr) ? izquierda : i->siguiente;
+
+            if (i == nullptr) {
+            i = izquierda;
+            } else {i = i->siguiente;}
+
             intercambiar(i, j);
         }
     }
-    i = (i == nullptr) ? izquierda : i->siguiente;
+
+    if (i == nullptr) {
+        i = izquierda;
+    } else {
+        i = i->siguiente;
+        }
+
     intercambiar(i, derecha);
     return i;
 }
 
+//Función que permite buscar un nodo segun en que posicion de la lista ligada se encuentra
 Nodo* ListaLigada::buscarNodo(int n){
-
     Nodo* nodoActual = this->cabeza;
-
     while(nodoActual->index < n){
         nodoActual = nodoActual->siguiente;
     }
@@ -147,56 +184,56 @@ Nodo* ListaLigada::buscarNodo(int n){
 }
 
 Nodo* ListaLigada::mergeSort(Nodo* cabeza) {
-        // Caso base: si la lista es vacía o tiene solo un nodo, está ordenada.
+        // Caso base: si la lista es vacía o tiene solo un nodo, está ordenada
         if (!cabeza || !cabeza->siguiente) {
             return cabeza;
         }
 
-        // Encuentra el nodo del medio para dividir la lista en dos mitades.
+        // Encuentra el nodo del medio para dividir la lista en dos mitades
         Nodo* mitad = obtenerMitad(cabeza);
-        Nodo* siguienteDeMitad = mitad->siguiente;  // Apunta al nodo que sigue al medio.
-        mitad->siguiente = nullptr;  // Divide la lista en dos partes cortando el enlace.
+        Nodo* siguienteDeMitad = mitad->siguiente;  // Apunta al nodo que sigue al medio
+        mitad->siguiente = nullptr;  // Divide la lista en dos partes cortando el enlace
 
-        // Ordena recursivamente ambas mitades.
+        // Ordena recursivamente ambas mitades
         Nodo* izquierda = mergeSort(cabeza);
         Nodo* derecha = mergeSort(siguienteDeMitad);
 
-        // Combina las dos mitades ordenadas en una lista ordenada.
+        // Combina las dos mitades ordenadas en una lista ordenada
         return merge(izquierda, derecha);
     }
 
     Nodo* ListaLigada::obtenerMitad(Nodo* cabeza) {
-        // Caso base: si la lista es vacía, devuelve el mismo nodo.
+        // Caso base: si la lista es vacía, devuelve el mismo nodo
         if (!cabeza) return cabeza;
 
-        // Inicializa dos punteros, mitad y rápido.
+        // Inicializa dos punteros, mitad y rápido
         Nodo* mitad = cabeza;
         Nodo* fin = cabeza->siguiente;
 
-        // Avanza mitad un nodo a la vez y rápido dos nodos a la vez.
+        // Avanza mitad un nodo a la vez y rápido dos nodos a la vez
         while (fin && fin->siguiente) {
-            mitad = mitad->siguiente;  // mitad avanza un nodo.
-            fin = fin->siguiente->siguiente;  // Rápido avanza dos nodos.
+            mitad = mitad->siguiente;  // mitad avanza un nodo
+            fin = fin->siguiente->siguiente;  // Rápido avanza dos nodos
         }
 
-        // mitad estará en la mitad de la lista cuando rápido llegue al final.
+        // mitad estará en la mitad de la lista cuando rápido llegue al final
         return mitad;
     }
 
     Nodo* ListaLigada::merge(Nodo* izquierda, Nodo* derecha) {
-        // Si una de las listas está vacía, devuelve la otra lista.
+        // Si una de las listas está vacía, devuelve la otra lista
         if (!izquierda) return derecha;
         if (!derecha) return izquierda;
 
         // Compara los datos en los nodos de ambas listas.
         if (izquierda->data.getAtributo() <= derecha->data.getAtributo()) {
-            // Si el nodo de la izquierda es menor o igual, sigue combinando el siguiente nodo de la izquierda con la lista derecha.
+            // Si el nodo de la izquierda es menor o igual, sigue combinando el siguiente nodo de la izquierda con la lista derecha
             izquierda->siguiente = merge(izquierda->siguiente, derecha);
-            return izquierda;  // Devuelve el nodo izquierdo como cabeza de la lista combinada.
+            return izquierda;  // Devuelve el nodo izquierdo como cabeza de la lista combinada
         } else {
-            // Si el nodo de la derecha es menor, sigue combinando la lista izquierda con el siguiente nodo de la derecha.
+            // Si el nodo de la derecha es menor, sigue combinando la lista izquierda con el siguiente nodo de la derecha
             derecha->siguiente = merge(izquierda, derecha->siguiente);
-            return derecha;  // Devuelve el nodo derecho como cabeza de la lista combinada.
+            return derecha;  // Devuelve el nodo derecho como cabeza de la lista combinada
         }
     }
 
--- main.cpp
+++ main.cpp
@@ -1,28 +1,39 @@
-
+//librerías
 #include <iostream>
 #include "listaligada.h"
 #include "producto.h"
 #include "procesos.h"
 
+//main
 int main() {
     
+    //inicializa una semilla de números aleatorios usando la función time donde transforma el tipo de dato
+    //time devuelve los segundos actuales desde Epoch 1 enero 1970
+    //static cast<unsigned int> transformael valor devuelto por nullptr de unsigned int a time_t
     srand(static_cast<unsigned int>(time(nullptr)));
 
+    //creacion de la lista
     ListaLigada lista;
 
+    //este objeto usa métodos para generar lso registros que luego se guardan en los nodos
     Procesos generador;
     cout << "Buenos dias" << endl;
 
-    cout << "Ingrese cantidad de registros que desea crear" << endl;
+    //n = cantidad de registros
+    cout << "Ingrese cantidad de registros que desea crear (muchos registros toman gran tiempo)" << endl;
     int n;
     cin >> n;
+
+    //un ciclo que agrega los nodos donde se pasa al constructor de la clase un objeto de tipo Producto que usa los métodos para generar un registro
     for (int i=0;i<n;++i){
         lista.agregar(Producto(generador.generarNumEntero(1, 250000), generador.generarNombre(), generador.generarNumEntero(1, 100), generador.generarNumEntero(100, 200000), 15072024), i);
     }
 
+    //muestra la lista
     cout<< "A continuacion se muestra la lista ya creada"<< endl;
     lista.imprimir();
 
+    //opiones accesibles
     cout << "Ingrese que atributo desea comparar" << endl;
     cout <<"0: Precio, 1: Código" << endl;
     int num;
@@ -33,10 +44,12 @@ int main() {
     int select;
     cin >> select;
 
+    //establece el atributo estático que le indica a todos los registros como que se tienen que ordenar
     lista.getCabeza()->data.setAtributo(num);
 
 
-
+    //ee utiliza el tipo de dato long long para almacenar la cantidad de milisegundos y así evitar errores
+    //la duración será el retorno de la funcion sort que ordena la lista segun la selección del usuario y retorna la diferencia en milisegundos entre ambos tiempos
     long long duracion = lista.sort(select);
 
     cout << "Lista ya ordenada" << endl;
