ebecd8f55ba73e84d73bf9e7f0b6dddfbbd351a0
valenciavm <valenciavm05@gmail.com>
2024-08-21 10:19:49 -0500

    comentarios quicksort

--- listaligada.cpp
+++ listaligada.cpp
@@ -128,45 +128,88 @@ Nodo* ListaLigada::getCabeza(){
     return cabeza;
 }
 
-//funció auxiliar para ejecutar la función recursiva
-void ListaLigada::quicksort() {
+// Función auxiliar para ejecutar la función recursiva
+void ListaLigada::quicksort()
+{
     quicksortrec(cabeza, final);
+    // llama a la función recursiva quicksortrec
+    // pasando como parámetros el primer nodo (cabeza) y el último nodo (final) de la lista ligada
 }
 
-//función recursiva quicksort, se usan dos indicadores izquierda y derecha 
-void ListaLigada::quicksortrec(Nodo* izquierda, Nodo* derecha) {
-    if (derecha != nullptr && izquierda != derecha && izquierda != derecha->siguiente) {
-        Nodo* pivote = dividir(izquierda, derecha);
+// Función recursiva quicksort, se usan dos indicadores izquierda y derecha
+void ListaLigada::quicksortrec(Nodo *izquierda, Nodo *derecha)
+{
+    // verifica que la lista no esté vacía, que izquierda no sea igual a derecha
+    // y que izquierda no sea el nodo siguiente a derecha (caso base para detener la recursión)
+    if (derecha != nullptr && izquierda != derecha && izquierda != derecha->siguiente)
+    {
+
+        Nodo *pivote = dividir(izquierda, derecha);
+        // llama a la función dividir para colocar los elementos menores al pivote a su izquierda
+        // y los mayores a su derecha, luego devuelve el nuevo pivote
+
         quicksortrec(izquierda, pivote->anterior);
+        // llama recursivamente a quicksortrec para ordenar la sublista a la izquierda del pivote
+
         quicksortrec(pivote->siguiente, derecha);
+        // llama recursivamente a quicksortrec para ordenar la sublista a la derecha del pivote
     }
 }
 
-//Divide la lista
-Nodo* ListaLigada::dividir(Nodo* izquierda, Nodo* derecha) {
-    Nodo* pivote = derecha; //se elige ultimo nodo como pivote
-    Nodo* i = izquierda->anterior;//inicializa i como el nodo anterior a la izquierda
+// Divide la lista en torno al pivote y coloca los elementos menores a su izquierda
+// y los mayores a su derecha
+Nodo *ListaLigada::dividir(Nodo *izquierda, Nodo *derecha)
+{
+    Nodo *pivote = derecha;
+    // elige el último nodo de la sublista como pivote
 
-    //recorre la lista desde izquierda hasta antes de la derecha
-    for (Nodo* j = izquierda; j != derecha; j = j->siguiente) {
-        if (j->data.getAtributo() <= pivote->data.getAtributo()) {
+    Nodo *i = izquierda->anterior;
+    // inicializa el puntero i como el nodo anterior a la izquierda
+    // para rastrear la posición donde colocar el siguiente elemento menor o igual al pivote
 
-            if (i == nullptr) {
-            i = izquierda;
-            } else {i = i->siguiente;}
+    // recorre la lista desde el nodo izquierdo hasta el nodo anterior al derecho
+    for (Nodo *j = izquierda; j != derecha; j = j->siguiente)
+    {
+
+        // si el valor del nodo actual (j) es menor o igual al valor del pivote
+        if (j->data.getAtributo() <= pivote->data.getAtributo())
+        {
+
+            if (i == nullptr)
+            {
+                i = izquierda;
+                // si i es nullptr (es decir, estamos al inicio de la lista),
+                // lo establece en el nodo izquierdo
+            }
+            else
+            {
+                i = i->siguiente;
+                // si i ya está en un nodo válido, avanza al siguiente nodo
+            }
 
             intercambiar(i, j);
+            // intercambia los valores entre los nodos i y j
+            // colocando el nodo menor o igual al pivote en su posición correcta
         }
     }
 
-    if (i == nullptr) {
+    if (i == nullptr)
+    {
         i = izquierda;
-    } else {
+        // si i aún es nullptr, se establece en el nodo izquierdo
+    }
+    else
+    {
         i = i->siguiente;
-        }
+        // si i está en un nodo válido, avanza al siguiente nodo
+    }
 
     intercambiar(i, derecha);
+    // finalmente, intercambia el nodo en i con el pivote,
+    // colocando el pivote en su posición correcta
+
     return i;
+    // devuelve la posición del pivote después de la partición
 }
 
 //Función que permite buscar un nodo segun en que posicion de la lista ligada se encuentra
